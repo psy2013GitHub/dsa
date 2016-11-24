f93de7d58fe5ea7972cc3a2cffba184c845c3b20
denzhou <superdengzhou@163.com>
2016-11-24 22:29:26 +0800

    leetcode distinct substring

--- string/leetcode_distinct_substring.py
+++ string/leetcode_distinct_substring.py
@@ -2,95 +2,28 @@
 __author__ = 'flappy'
 
 class Solution(object):
+    class Solution(object):
+        def numDistinct(self, s, t):
+            """
+            :type s: str
+            :type t: str
+            :rtype: int
+            """
+            if len(s) < len(t):
+                return 0
 
-    def numDistinct(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: int
-        """
-        self.count, self.s, self.t = 1, s, t
-        self.brute_force(0, 0, 0)
-        return self.count
+            state_mat = [[0, ] * (len(s) + 1) for _ in range(len(t) + 1)]
+            state_mat[0][0] = 1
+            for i in range(len(s)):
+                state_mat[0][i + 1] = 1
 
-    def brute_force(self, i, j, curr_count):
-        '''
-            从左到右匹配, 直到匹配到j为止, 遇到重复得计算组合数。
-            time complexity: O(n^2)
-        :param i:
-        :param j:
-        :param curr_count:
-        :return:
-        '''
-        if i >= len(self.t):
-            if self.count == 0:
-                self.count = 1
-            return
-
-        if j >= len(self.t):
-            candidate = 1
-            while i < len(self.s) and i > 0 and self.s[i] == self.s[i-1]:
-                print('---***', i, j, self.s[:i+1], self.t[:j+1], candidate)
-                candidate += 1
-                i += 1
-            if candidate > 1:
-                self.count *= self.C(candidate, 1)
-            self.brute_force(i, 0, curr_count)
-
-        # print(1)
-        while i < len(self.s):
-            if self.s[i] != self.t[j]:
-                i += 1
-            else:
-                break
-
-        # print(2)
-        if i >= len(self.s) and not j >= len(self.t):
-            self.count = 0
-            return
-
-        # print(3)
-        # print(i, j, self.s[:i+1], self.t[:j+1])
-        raw_i, raw_j, necessary_repeat, candidate, cum_prod = i, j, 0, 0, 0
-        while i < len(self.s) and j < len(self.t):
-            if self.s[i] == self.t[j]:
-                if j > 0 and self.t[j] == self.t[j-1]:
-                    necessary_repeat += 1
-                i += 1
-                j += 1
-            else:
-                necessary_repeat += 1
-                candidate = necessary_repeat
-                while i > 0 and self.s[i] == self.s[i-1]:
-                    # print('***', i, j, self.s[:i+1], self.t[:j+1], candidate)
-                    candidate += 1
-                    i += 1
-
-                print('***', i, j, self.s[:i+1], self.t[:j+1], candidate, necessary_repeat)
-
-
-                if candidate > 1:
-                    self.count *= self.C(candidate, necessary_repeat)
-
-
-                # print('...', i, j, self.s[:i+1], self.t[:j+1])
-                break
-
-        # print(4)
-        self.brute_force(i, j, curr_count)
-
-    def C(self, m, n):
-        '''
-            calc `latex: C_{m}^n`
-        :param m:
-        :param n:
-        :return:
-        '''
-        res = 1
-        while m > n:
-            res *= m
-            m -= 1
-        return res
+            for j in range(len(t)):
+                for i in range(len(s)):
+                    if t[j] == s[i]:
+                        state_mat[j + 1][i + 1] = state_mat[j][i] + state_mat[j + 1][i]
+                    else:
+                        state_mat[j + 1][i + 1] = state_mat[j + 1][i]
+            return state_mat[-1][-1]
 
 def test():
     solution = Solution()
