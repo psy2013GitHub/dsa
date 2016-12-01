5713a3024d395e7a7a1bd728f61f718ad3f895d8
deng.zhou <deng.zhou@immomo.com>
2016-12-01 15:45:22 +0800

    leetcode

--- string/leetcode_integer_to_english_words.py
+++ string/leetcode_integer_to_english_words.py
@@ -75,10 +75,28 @@ class Solution(object):
 
         return ' '.join(res[::-1])
 
+    def concise(self, num):
+        to19 = 'One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve ' \
+               'Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen'.split()
+        tens = 'Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety'.split()
+        def words(n):
+            if n < 20:
+                return to19[n-1:n]
+            if n < 100:
+                return [tens[n/10-2]] + words(n%10)
+            if n < 1000:
+                return [to19[n/100-1]] + ['Hundred'] + words(n%100)
+            for p, w in enumerate(('Thousand', 'Million', 'Billion'), 1):
+                # print(p, w)
+                if n < 1000**(p+1):
+                    print(p, w, 1000**(p+1), n/1000**p, n%1000**p)
+                    return words(n/1000**p) + [w] + words(n%1000**p)
+        return ' '.join(words(num)) or 'Zero'
+
 def test():
     solution = Solution()
     def Print(num):
-        print(num, solution.numberToWords(num))
+        print(num, solution.concise(num))
     Print(0)
     Print(123)
     Print(12345)
@@ -92,6 +110,7 @@ def test():
     Print(1000001)
     Print(1010101)
     Print(1000000)
+    Print(1000100100)
 
 if __name__ == '__main__':
     test()
